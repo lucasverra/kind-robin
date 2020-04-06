8208daec3b390d1fbea5f0193baa133646636007
blackjmxx <alandou.jeanmichel@gmail.com>
2020-04-06 16:43:48 +0200

    add one signl to oroute

--- src/html.js
+++ src/html.js
@@ -14,6 +14,7 @@ export default function HTML(props) {
                     OneSignal.init({
                       appId: "2267b1d7-ec2c-4e4e-8ded-ade2a7ff194d",
                     });
+                    OneSignal.setDefaultNotificationUrl("https://kind-robin-60456.netlify.com/profile");
                     //OneSignal.setEmail("a@b.com");
                     //OneSignal.sendTag("toto", "titi"); 
                   });
