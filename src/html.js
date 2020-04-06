3f537b680650ea9ddee03089df52971ac543c032
blackjmxx <alandou.jeanmichel@gmail.com>
2020-04-06 17:22:07 +0200

    test

--- src/html.js
+++ src/html.js
@@ -9,15 +9,7 @@ export default function HTML(props) {
       <script
         dangerouslySetInnerHTML={{
           __html: `
-              var OneSignal = window.OneSignal || [];
-                  OneSignal.push(function() {
-                    OneSignal.init({
-                      appId: "2267b1d7-ec2c-4e4e-8ded-ade2a7ff194d",
-                    });
-                    OneSignal.setDefaultNotificationUrl("https://kind-robin-60456.netlify.com/profile");
-                    //OneSignal.setEmail("a@b.com");
-                    //OneSignal.sendTag("toto", "titi"); 
-                  });
+              
               `,
         }}
       />
--- src/templates/profile.js
+++ src/templates/profile.js
@@ -5,7 +5,15 @@ import {isLoggedIn} from '../services/auth';
 
 export default class Profile extends React.Component {
     componentDidMount(){
-       
+        var OneSignal = window.OneSignal || [];
+        OneSignal.push(function() {
+        OneSignal.init({
+            appId: "2267b1d7-ec2c-4e4e-8ded-ade2a7ff194d",
+          });
+          OneSignal.setDefaultNotificationUrl("https://kind-robin-60456.netlify.com/profile");
+          //OneSignal.setEmail("a@b.com");
+          //OneSignal.sendTag("toto", "titi"); 
+        });
     }
 
     handlePreference = (preferences) =>{
