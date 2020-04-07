f8bea25906a9cdab037df1ecf2f057ba5f559b35
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2020-04-07 18:49:33 +0500

    testing two

--- src/templates/profile.js
+++ src/templates/profile.js
@@ -29,9 +29,6 @@ export default class Profile extends React.Component {
       //OneSignal.setEmail("a@b.com");
       //OneSignal.sendTag("toto", "titi");
     });
-    if (!isLoggedIn()) {
-      if (typeof window !== `undefined`) window.location.replace(`/Login`);
-    }
   }
   render() {
     const options = [
@@ -53,147 +50,150 @@ export default class Profile extends React.Component {
       "21h",
       "22h",
     ];
-
-    return (
-      <Layout {...this.props}>
-        <section className="custom-container-2">
-          <form
-            name="myprefrences"
-            method="POST"
-            className="fmyprefrences-form"
-            onSubmit={(event) => {
-              this.handleSubmit(event);
-            }}
-          >
-            <div className="form-row">
-              <label>
-                Nom
-                <span className="screen-reader-text">Nom</span>
-              </label>
-              <div className="input_container">
-                <input
-                  className="name"
-                  type="text"
-                  name="name"
-                  required
-                  autoFocus
-                />
+    if (!isLoggedIn()) {
+      if (typeof window !== `undefined`) window.location.replace(`/Login`);
+    } else {
+      return (
+        <Layout {...this.props}>
+          <section className="custom-container-2">
+            <form
+              name="myprefrences"
+              method="POST"
+              className="fmyprefrences-form"
+              onSubmit={(event) => {
+                this.handleSubmit(event);
+              }}
+            >
+              <div className="form-row">
+                <label>
+                  Nom
+                  <span className="screen-reader-text">Nom</span>
+                </label>
+                <div className="input_container">
+                  <input
+                    className="name"
+                    type="text"
+                    name="name"
+                    required
+                    autoFocus
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="form-row">
-              <label>
-                A quelle heure souhaiteriez-vous faire de l'exercice chaque
-                jour?
-                <span className="screen-reader-text">
+              <div className="form-row">
+                <label>
                   A quelle heure souhaiteriez-vous faire de l'exercice chaque
                   jour?
-                </span>
-              </label>
-              <div className="input_container">
-                <select id="time" name="time" className="time">
-                  <option></option>
-                  {options.map((value, index) => {
-                    return (
-                      <option key={index} value={value}>
-                        {value}
-                      </option>
-                    );
-                  })}
-                </select>
+                  <span className="screen-reader-text">
+                    A quelle heure souhaiteriez-vous faire de l'exercice chaque
+                    jour?
+                  </span>
+                </label>
+                <div className="input_container">
+                  <select id="time" name="time" className="time">
+                    <option></option>
+                    {options.map((value, index) => {
+                      return (
+                        <option key={index} value={value}>
+                          {value}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
               </div>
-            </div>
 
-            <div className="form-row">
-              <label>
-                Quel est votre objectif d’activité physique?
-                <span className="screen-reader-text">
+              <div className="form-row">
+                <label>
                   Quel est votre objectif d’activité physique?
-                </span>
-              </label>
-              <div className="input_container">
-                <select id="goals" name="goals" className="goals">
-                  <option></option>
-                  <option value="Se musler">Se musler</option>
-                  <option value="Se détendre">Se détendre</option>
-                  <option value="Se défouler">Se défouler</option>
-                </select>
+                  <span className="screen-reader-text">
+                    Quel est votre objectif d’activité physique?
+                  </span>
+                </label>
+                <div className="input_container">
+                  <select id="goals" name="goals" className="goals">
+                    <option></option>
+                    <option value="Se musler">Se musler</option>
+                    <option value="Se détendre">Se détendre</option>
+                    <option value="Se défouler">Se défouler</option>
+                  </select>
+                </div>
               </div>
-            </div>
-            <div className="form-row">
-              <label>
-                Souhaitez vous recevoir des notification push?
-                <span className="screen-reader-text">
+              <div className="form-row">
+                <label>
                   Souhaitez vous recevoir des notification push?
-                </span>
-              </label>
-              <div className="input_container">
-                <ToggleButton
-                  inactiveLabel="no"
-                  activeLabel="yes"
-                  value={this.state.notifications || false}
-                  onToggle={(value) => {
-                    this.setState({
-                      notifications: !this.state.notifications,
-                    });
-                  }}
-                />
+                  <span className="screen-reader-text">
+                    Souhaitez vous recevoir des notification push?
+                  </span>
+                </label>
+                <div className="input_container">
+                  <ToggleButton
+                    inactiveLabel="no"
+                    activeLabel="yes"
+                    value={this.state.notifications || false}
+                    onToggle={(value) => {
+                      this.setState({
+                        notifications: !this.state.notifications,
+                      });
+                    }}
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="form-row">
-              <label>
-                Souhaitez vous recevoir des contenus adaptés aux enfants?
-                <span className="screen-reader-text">
+              <div className="form-row">
+                <label>
                   Souhaitez vous recevoir des contenus adaptés aux enfants?
-                </span>
-              </label>
-              <div className="input_container">
-                <ToggleButton
-                  inactiveLabel="no"
-                  activeLabel="yes"
-                  value={this.state.content || false}
-                  onToggle={(value) => {
-                    this.setState({
-                      content: !this.state.content,
-                    });
-                  }}
-                />
+                  <span className="screen-reader-text">
+                    Souhaitez vous recevoir des contenus adaptés aux enfants?
+                  </span>
+                </label>
+                <div className="input_container">
+                  <ToggleButton
+                    inactiveLabel="no"
+                    activeLabel="yes"
+                    value={this.state.content || false}
+                    onToggle={(value) => {
+                      this.setState({
+                        content: !this.state.content,
+                      });
+                    }}
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="form-row">
-              <label>
-                Souhaitez vous recevoir des notification email?
-                <span className="screen-reader-text">
+              <div className="form-row">
+                <label>
                   Souhaitez vous recevoir des notification email?
-                </span>
-              </label>
-              <div className="input_container">
-                <ToggleButton
-                  inactiveLabel="no"
-                  activeLabel="yes"
-                  value={this.state.emails || false}
-                  onToggle={(value) => {
-                    this.setState({
-                      emails: !this.state.emails,
-                    });
-                  }}
-                />
+                  <span className="screen-reader-text">
+                    Souhaitez vous recevoir des notification email?
+                  </span>
+                </label>
+                <div className="input_container">
+                  <ToggleButton
+                    inactiveLabel="no"
+                    activeLabel="yes"
+                    value={this.state.emails || false}
+                    onToggle={(value) => {
+                      this.setState({
+                        emails: !this.state.emails,
+                      });
+                    }}
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="btn-container">
-              <button className="cancel-btn" type="submit">
-                annuler
-              </button>
-              <button className="submit-btn" type="submit">
-                sauvegarder
-              </button>
-            </div>
-          </form>
-        </section>
-      </Layout>
-    );
+              <div className="btn-container">
+                <button className="cancel-btn" type="submit">
+                  annuler
+                </button>
+                <button className="submit-btn" type="submit">
+                  sauvegarder
+                </button>
+              </div>
+            </form>
+          </section>
+        </Layout>
+      );
+    }
   }
 }
