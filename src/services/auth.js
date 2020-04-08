a50821bf2c9060cc32ee9fa0a36d7d00add10f86
blackjmxx <alandou.jeanmichel@gmail.com>
2020-04-08 22:14:27 +0200

    handle tag

--- src/services/auth.js
+++ src/services/auth.js
@@ -20,7 +20,58 @@ export const setPrefrence = (userPreference) => {
     if(User.current()){
         User.current().set({userPreference})
         User.current().save()
-            .then(x => console.log(x))
+            .then(x => {
+                var OneSignal = window.OneSignal || [];
+                let tags = {}     
+
+                OneSignal.deleteTags(['physicalActivityGoal', 'hasNotificationsPush', 'hasNotificationsEmail', 'hasChildFriendlyContent']).then(() => {
+
+                    if(userPreference.hasNotificationsPush){
+                        tags.hasNotificationsPush  = 'hasNotificationsPush'
+                    } 
+                    if (userPreference.hasNotificationsEmail) {
+                        tags.hasNotificationsEmail  = 'hasNotificationsEmail'
+                    } 
+                    if(userPreference.hasChildFriendlyContent) {
+                        tags.hasChildFriendlyContent  = 'hasChildFriendlyContent'
+                    }
+                    tags.physicalActivityGoal = userPreference.physicalActivityGoal.value
+
+                    OneSignal.sendTags(tags);
+                })
+
+
+                // OneSignal.push(function() {
+                //   let tags = {}     
+                // //   OneSignal.getUserId(function(userId) {
+                // //     console.log("OneSignal User ID:", userId);
+                // //     // (Output) OneSignal User ID: 270a35cd-4dda-4b3f-b04e-41d7463a2316    
+                // //   });
+
+
+                // //     if(userPreference.hasNotificationsPush){
+                // //         tags.hasNotificationsPush = 'Notification'
+                // //     } else if (userPreference.hasNotificationsEmail) {
+  
+                // //     } else if(userPreference.hasChildFriendlyContent) {
+  
+                // //     }
+                
+                // //.then(r => { 
+
+                // //     if(userPreference.hasNotificationsPush){
+                // //         tags.hasNotificationsPush = 'Notification'
+                // //     } else if (userPreference.hasNotificationsEmail) {
+  
+                // //     } else if(userPreference.hasChildFriendlyContent) {
+  
+                // //     }
+                // //     OneSignal.sendTags({});
+                // //   })
+                //   //OneSignal.setEmail("a@b.com");
+                //   //OneSignal.sendTag("toto", "titi"); 
+                // });
+            })
             .catch( err => console.log(err))
     }
 };
