66e74491e0e24490bdb2f7e772a002cada6ea4df
Tall Mamadou Lamine <mtall1432@gmail.com>
2020-04-11 18:29:02 +0200

    PWA configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,75 +1,72 @@
 module.exports = {
-    pathPrefix: '/',
-    siteMetadata: require('./site-metadata.json'),
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-source-data`,
-        `gatsby-plugin-sass`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `pages`,
-                path: `${__dirname}/src/pages`,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-stackbit-static-sass`,
-            options: {
-                inputFile: `${__dirname}/src/sass/main.scss`,
-                outputFile: `${__dirname}/public/assets/css/main.css`
-            },
-        },
-    
-        {
-            resolve: `gatsby-plugin-sharp`,
-            options: {
-              useMozJpeg: false,
-              stripMetadata: true,
-              defaultQuality: 75,
-            }
-        },
+  pathPrefix: "/",
+  siteMetadata: require("./site-metadata.json"),
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-source-data`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-stackbit-static-sass`,
+      options: {
+        inputFile: `${__dirname}/src/sass/main.scss`,
+        outputFile: `${__dirname}/public/assets/css/main.css`,
+      },
+    },
 
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [`gatsby-remark-component`]
-            }
-        },
-        {
-            resolve: `gatsby-remark-embedder`,
-            options: {
-              customTransformers: [
-                
-              ],
-              services: {
-                // The service-specific options by the name of the service
-              },
-            },
-          },
-        {
-            resolve: `gatsby-remark-page-creator`,
-            options: {
-                
-            }
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-              name: `Bouger Chez Vous`,
-              short_name: `Bouger Chez Vous`,
-              start_url: `/`,
-              icon:`src/favicon.png`
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        useMozJpeg: false,
+        stripMetadata: true,
+        defaultQuality: 75,
+      },
+    },
 
-             
-            },
-          },
-        {
-            resolve: `@stackbit/gatsby-plugin-menus`,
-            options: {
-                sourceUrlPath: `fields.url`,
-                pageContextProperty: `menus`,
-                menus: require('./src/data/menus.json'),
-            }
-        }
-    ]
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-component`],
+      },
+    },
+    {
+      resolve: `gatsby-remark-embedder`,
+      options: {
+        customTransformers: [],
+        services: {
+          // The service-specific options by the name of the service
+        },
+      },
+    },
+    {
+      resolve: `gatsby-remark-page-creator`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Bouger Chez Vous`,
+        short_name: `Bouger Chez Vous`,
+        start_url: `/`,
+        icon: `src/favicon.png`,
+        display: `standalone`,
+        background_color: `#2672ff`,
+        theme_color: `#2672ff`,
+      },
+    },
+    {
+      resolve: `@stackbit/gatsby-plugin-menus`,
+      options: {
+        sourceUrlPath: `fields.url`,
+        pageContextProperty: `menus`,
+        menus: require("./src/data/menus.json"),
+      },
+    },
+  ],
 };
