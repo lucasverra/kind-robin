5150e2dc7f58bdaacd4e391849a3b95ee7b19c1d
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2020-04-12 20:44:24 +0500

    removed offline plugin from site

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,7 +1,1 @@
-importScripts('https://kind-robin-60456.netlify.com/sw.js');
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
-
-// If service workers are supported, and one isn't already registered
-if ('serviceWorker' in navigator && !navigator.serviceWorker.controller) {
-    navigator.serviceWorker.register('/OneSignalSDKWorker.js');
-  }
+importScripts("https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js");
