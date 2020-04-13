e7cbb949b6247928b796e1742a5e87164a4a22e3
Lucas Verra <lucasverra@users.noreply.github.com>
2020-04-13 14:33:49 +0200

    fix plugin install

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
               },
             },
           },
-          [`gatsby-remark-component`]
+          `gatsby-remark-component`,
           // Other plugins here...
         ],
       },
