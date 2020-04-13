fbdf5cc4fcc16c8517118979e32b4fdbbcf991ee
blackjmxx <alandou.jeanmichel@gmail.com>
2020-04-13 19:43:13 +0200

    update env one signal

--- src/templates/profile.js
+++ src/templates/profile.js
@@ -199,11 +199,11 @@ export default class Profile extends React.Component {
                       var OneSignal = window.OneSignal || [];
                       OneSignal.push(function () {
                         OneSignal.init({
-                          appId: "2267b1d7-ec2c-4e4e-8ded-ade2a7ff194d",
+                          appId: `${process.env.ONE_SIGNAL_APP_ID}`,
                           allowLocalhostAsSecureOrigin: true,
                         });
                         OneSignal.setDefaultNotificationUrl(
-                          "https://kind-robin-60456.netlify.com/profile"
+                          `${process.env.ONE_SIGNAL_URL_START}`
                         );
                         //OneSignal.setEmail("a@b.com");
                         //OneSignal.sendTag("toto", "titi");
